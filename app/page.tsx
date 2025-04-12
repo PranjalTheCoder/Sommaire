431ccb9ddea624b473d22d8195d413b4d3bf41b9
PranjalTheCoder <pranjaltamrakar0794@gmail.com>
2025-04-13 02:16:44 +0530

    Hero section complete

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
+import BgGradient from "@/components/common/bg-gradient";
 import "./globals.css";
 import HeroSection from "@/components/home/hero-section";
+import DemoSection from "@/components/home/demo-section";
 
 export default function Home() {
   return (
     <div className="relative w-full">
-      <HeroSection />
-      {/* <DemoSection /> */}
+      <BgGradient />
+      <div className="flex flex-col">
+        <HeroSection />
+        <DemoSection />
+      </div>
       {/* <HowItWorksSection /> */}
       {/* <PricingSection /> */}
       {/* <CTASection /> */}
