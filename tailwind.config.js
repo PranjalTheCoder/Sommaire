cd0fc843f4d764ab5a383b68826ced48054ddd4c
PranjalTheCoder <pranjaltamrakar0794@gmail.com>
2025-04-12 17:08:55 +0530

    works on Navigation

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+// tailwind.config.js
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-geist-sans)", ...fontFamily.sans],
+        mono: ["var(--font-geist-mono)", ...fontFamily.mono],
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")], // ✅ Add plugin here
+};
