a7fb82e32e8c8fa68fb5b412fcace258407fb2d4
PranjalTheCoder <pranjaltamrakar0794@gmail.com>
2025-06-16 14:15:51 +0530

    change domain

--- app/(logged-in)/upload/page.tsx
+++ app/(logged-in)/upload/page.tsx
@@ -1,8 +1,24 @@
 import BgGradient from "@/components/common/bg-gradient";
 import UploadForm from "@/components/upload/upload-form";
 import { UploadHeader } from "@/components/upload/upload-header";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-export default function Page() {
+
+
+export default async function Page() {
+  const user = await currentUser();
+  if(!user?.id){
+    redirect("/sign-in");
+  }
+  const userId = user.id;
+  
+  const { hasReachedLimit } = await import ("@/lib/user");
+  if(!hasReachedLimit(userId)){
+    redirect("/dashboard");
+  } 
+  
+
   return (
     <section className="min-h-screen">
       <BgGradient />
